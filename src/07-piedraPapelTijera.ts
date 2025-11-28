087407074dc3f762dc14f57359530a066db9f165
AlvaritoGSG <AlvaritoGSG@outlook.com>
2025-11-28 00:33:49 -0400

    Ejercicio 7 (piedra papel tijera) Render resultados-victorias

--- src/07-piedraPapelTijera.ts
+++ src/07-piedraPapelTijera.ts
@@ -1,3 +1,4 @@
+let pcChoice: string;
 let winner: string | EventListenerOrEventListenerObject;
 let victories: string[] = [];
 
@@ -17,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   ] as HTMLButtonElement[];
 
   const getWinner = (userChoice: string): string => {
-    const pcChoice = getRandomChoice();
+    pcChoice = getRandomChoice();
     if (userChoice === pcChoice) {
       victories.push("draw");
     } else if (userChoice === "rockOption") {
@@ -38,22 +39,125 @@ document.addEventListener("DOMContentLoaded", () => {
 
   rockBtn.addEventListener("click", () => {
     winner = getWinner("rockOption");
-    console.log(winner);
+    renderResults("rockOption");
   });
   paperBtn.addEventListener("click", () => {
     winner = getWinner("paperOption");
-    console.log(victories);
-    p?.classList.remove("d-none");
-    p.innerHTML = `Resultado final: ${victories.toString()}`;
-    p.textContent = `VICTORIAS: ${victories}`;
+    renderResults("paperOption");
   });
   scissorsBtn.addEventListener("click", () => {
     winner = getWinner("scissorsOption");
-    console.log(victories);
-    alert(victories.length);
+    renderResults("scissorsOption");
   });
 
-  const p = document.querySelector(
+  const pInitialHelp = document.querySelector(
     "#helperContainer p"
   ) as HTMLParagraphElement;
+  const resultContainer = document.getElementById(
+    "resultContainer"
+  ) as HTMLDivElement;
+  const [pcSelectionOutput, userSelectionOutput] =
+    resultContainer.querySelectorAll(
+      "p output"
+    ) as NodeListOf<HTMLOutputElement>;
+  const resultOutput = document.getElementById(
+    "resultOutput"
+  ) as HTMLOutputElement;
+
+  const scoreContainerTable = document.getElementById(
+    "scoreContainer"
+  ) as HTMLTableElement;
+  const [userVictoriesOutput, pcVictoriesOutput] =
+    scoreContainerTable.querySelectorAll(
+      "thead output"
+    ) as NodeListOf<HTMLOutputElement>;
+  const historyVictoriesOutput = scoreContainerTable.querySelector(
+    "tbody"
+  ) as HTMLTableSectionElement;
+
+  const drawsOutput = scoreContainerTable.querySelector(
+    "tfoot output"
+  ) as HTMLOutputElement;
+
+  const renderResults = (userChoice: string): void => {
+    // Render Choices
+    pInitialHelp.classList.add("d-none");
+    resultContainer.classList.remove("d-none");
+    // pcSelectionOutput.textContent =
+    //   pcChoice == "rockOption"
+    //     ? "ROCA"
+    //     : pcChoice == "paperOption"
+    //     ? "PAPEL"
+    //     : "TIJERAS";
+    pcSelectionOutput.textContent = pcChoice
+      .replace("Option", "")
+      .toUpperCase();
+    userSelectionOutput.textContent = userChoice
+      .replace("Option", "")
+      .toUpperCase();
+    // Render result
+    if (winner === "draw") {
+      resultOutput.classList.remove("bg-success", "bg-danger");
+      resultOutput.classList.add("bg-secondary");
+      resultOutput.textContent = "¡EMPATE!";
+    } else if (winner === "userWin") {
+      resultOutput.classList.remove("bg-secondary", "bg-danger");
+      resultOutput.classList.add("bg-success");
+      resultOutput.textContent = "¡GANASTE!";
+    } else if (winner === "pcWin") {
+      resultOutput.classList.remove("bg-success", "bg-secondary");
+      resultOutput.classList.add("bg-danger");
+      resultOutput.textContent = "¡GANÓ TU PC!";
+    }
+    // Render score
+    userVictoriesOutput.textContent = victories
+      .filter((v) => v === "userWin")
+      .length.toString();
+    pcVictoriesOutput.textContent = victories
+      .filter((v) => v === "pcWin")
+      .length.toString();
+    drawsOutput.textContent = victories
+      .filter((v) => v === "draw")
+      .length.toString();
+
+    // Render history
+    historyVictoriesOutput.classList.remove("d-none");
+    victories.forEach((v, i) => {
+      if (i + 1 > historyVictoriesOutput.rows.length) {
+        // historyVictoriesOutput.innerHTML += `<tr>
+        //   <td>${i + 1}</td>
+        //   <td>${victories.filter((val) => val === "userWin").length}</td>
+        //   <td>${victories.filter((val) => val === "pcWin").length}</td>
+        // </tr>`;
+
+        const newRow = historyVictoriesOutput.insertRow();
+        const cellIn = newRow.insertCell(0);
+        const cellUs = newRow.insertCell(1);
+        const cellPc = newRow.insertCell(2);
+
+        cellIn.textContent = (i + 1).toString();
+
+        cellUs.textContent = victories
+          .filter((val) => val === "userWin")
+          .length.toString();
+
+        cellPc.textContent = victories
+          .filter((val) => val === "pcWin")
+          .length.toString();
+      }
+    });
+  };
+
+  const cleanBtn = scoreContainerTable.querySelector(
+    "tfoot button"
+  ) as HTMLButtonElement;
+  cleanBtn.addEventListener("click", () => {
+    victories = [];
+    historyVictoriesOutput.innerHTML = "";
+    userVictoriesOutput.textContent = "0";
+    pcVictoriesOutput.textContent = "0";
+    drawsOutput.textContent = "0";
+    resultContainer.classList.add("d-none");
+    pInitialHelp.classList.remove("d-none");
+  });
 });
